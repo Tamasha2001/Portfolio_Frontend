de3dc4cdcf6debfa4f676b1af0e93b11fb57c269
Tamasha Kavindi <tamashakavindi674@gmail.com>
2025-10-19 09:58:59 +0530

    updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,7 @@
 import axios from "axios";
-const apiBaseUrl = import.meta.env.VITE_API_URL;
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
-
-fetch(`${apiBaseUrl}/projects`)
-  .then(res => res.json())
-  .then(data => console.log(data))
-  .catch(err => console.error("Error fetching projects:", err));
 
 export default API;
